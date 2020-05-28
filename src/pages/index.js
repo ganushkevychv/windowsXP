0537342caa2d8ceb8a7ef87fccc1023df2ddfb07
Vladyslav <ganushkevychv@gmail.com>
2020-05-28 17:42:05 +0200

    gatsby background image added to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import BackgrondImage from "gatsby-background-image"
+import "../styles/index.scss"
+import { useStaticQuery, graphql } from "gatsby"
 
-const IndexPage = () => (
+const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+<BackgrondImage className="background"
+fluid={props.data.windows.childImageSharp.fluid}>
+  
+</BackgrondImage>
+</Layout>
+  
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+query {
+  windows: file(relativePath:{ eq: "windows.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 1800) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`;
