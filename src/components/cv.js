f3a780f728a1d5d60de46b91e0c6314e5f7aaad7
Vladyslav <ganushkevychv@gmail.com>
2020-06-01 09:52:42 +0200

    download link corrected

--- src/components/cv.js
+++ src/components/cv.js
@@ -14,8 +14,8 @@ const CV = () => {
 
     return (
         <div>
-            <a style={cv} href="https://romantic-johnson-a557e4.netlify.app/cv/Vladyslav%20Ganushkevych_CV.pdf" download="Vladyslav Ganushkevych_CV.pdf">
-                My CV <img style={download} src={dl}/>
+            <a style={cv} href="../public/cv/Vladyslav Ganushkevych_CV.pdf" download="Vladyslav Ganushkevych_CV.pdf">
+                My CV <img style={download} src={dl} alt="download icon"/>
                 </a>
         </div>
     )
